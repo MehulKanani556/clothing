13414a3d4dde39ef65121d126c84ceccd5c74fb5
mehulkanani556 <129421658+MehulKanani556@users.noreply.github.com>
2026-01-02 09:08:02 +0530

    a

--- back/routes/indexRoutes.js
+++ back/routes/indexRoutes.js
@@ -5,6 +5,7 @@ const { upload } = require('../middleware/upload');
 const { createUser, verifyRegistration, login, logout, forgotPassword, verifyOtp, changePassword, generateNewToken } = require('../auth/auth');
 const { auth } = require('../middleware/auth');
 const { getAllUsers, getSingleUser, deleteUser, updateUser } = require('../controllers/user.controller');
+const { getAllProducts, getProductById, getRelatedProducts, createProduct } = require('../controllers/product.controller');
 
 // auth
 router.post('/register', upload.single("photo"), createUser);
@@ -21,5 +22,11 @@ router.get('/users', getAllUsers);
 router.get('/users/:id', getSingleUser);
 router.delete('/users/:id', auth, deleteUser);
 router.put('/users/:id', updateUser);
+
+// products
+router.get('/products', getAllProducts);
+router.post('/products', createProduct); // Potentially protect this with admin middleware later
+router.get('/products/:id', getProductById);
+router.get('/products/:id/related', getRelatedProducts);
 
 module.exports = router;
