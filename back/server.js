04d0945578ce07635f34400c371a144f13b9b077
Mehul Kanani <129421658+MehulKanani556@users.noreply.github.com>
2026-06-24 10:13:40 +0530

    Update server.js

--- back/server.js
+++ back/server.js
@@ -29,7 +29,7 @@ app.use(helmet());
 // app.use(limiter);
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://clothing-td68.onrender.com/],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
